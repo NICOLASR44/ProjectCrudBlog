080253477721f5089aae7ef5fe24ba43ebde6157
NICOLASR44 <158582207+NICOLASR44@users.noreply.github.com>
2024-10-10 14:34:11 +0200

    add pumkins and css

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,55 @@
+import React, { useState, useEffect } from "react";
 import BlogCards from "../components/BlogCard";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import "./styles/Home.css";
+import pumpkinImage from "../assets/images/pumpkins2.png"; // Chemin de l'image
 
 function Home() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  // Mise à jour de la position de la citrouille avec la position de la souris
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setMousePosition({
+        x: event.clientX, // Position X de la souris
+        y: event.clientY, // Position Y de la souris
+      });
+    };
+
+    // Ajout de l'écouteur d'événement de mouvement de la souris
+    window.addEventListener("mousemove", handleMouseMove);
+
+    // Nettoyage de l'écouteur d'événement lors du démontage du composant
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
     <>
       <div className="home__Navbar">
         <Navbar />
       </div>
+
+      {/* Ajout de la citrouille qui suit la souris */}
+      <div
+        className="home__pumpkin"
+        style={{
+          position: "fixed",
+          left: `${mousePosition.x + 20}px`, // Placer la citrouille 20px à droite de la souris
+          top: `${mousePosition.y - 75}px`, // Ajustement pour la position Y
+          transition: "transform 0.1s ease", // Animation pour lisser le suivi de la souris
+        }}
+      >
+        <img
+          src={pumpkinImage}
+          alt="Bouncing Pumpkin"
+          className="bouncing-pumpkin"
+          style={{ width: "150px" }}
+        />
+      </div>
+
       <div className="home__main">
         <div className="home__Title">
           <p className="home__Title-h1">Welcome, dear developer</p>
