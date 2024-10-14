d3b0327747014bd4bb75ad1e7aa0d03f17e0348a
NICOLASR44 <158582207+NICOLASR44@users.noreply.github.com>
2024-10-14 10:26:43 +0200

    css viewArticles modification

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import "./styles/AboutPage.css";
